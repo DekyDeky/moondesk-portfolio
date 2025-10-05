264156b4cd8a780b3b214dc752d6a8f007056b82
Ricardo Hideki Tsugikava Junior <166323985+DekyDeky@users.noreply.github.com>
2025-10-04 21:22:33 -0300

    commit errado

--- src/components/window/window.tsx
+++ src/components/window/window.tsx
@@ -1,3 +1,5 @@
+import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
+
 import Close from '../../assets/window/close.svg'
 
 import './window.scss';
@@ -11,24 +13,37 @@ interface windowProps {
 
 export default function Window({windowName, windowContent, width, height}: windowProps){
 
-
-
     const widthWindow: React.CSSProperties = {
         width,
         height,
     }
 
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+        id: 'draggable-window',
+    })
+
+    const windowStyle: React.CSSProperties = {
+        transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
+        position: 'absolute',
+    }
+
     return (
-        <main className='window'  style={widthWindow}>
-            <header className='window-tab'>
-                <h4 className='window-tab-name'>{windowName}</h4>
-                <img className='window-tab-close' src={Close} alt="" />
-            </header>
-            <div className="window-border">
-                <main className='window-content'>
-                    {windowContent}
-                </main>
-            </div>
-        </main>
+        <DndContext>
+            <main className='window' 
+            ref={setNodeRef}
+            {...listeners}
+            {...attributes}
+            style={{...widthWindow, ...windowStyle}}>
+                <header className='window-tab'>
+                    <h4 className='window-tab-name'>{windowName}</h4>
+                    <img className='window-tab-close' src={Close} alt="" />
+                </header>
+                <div className="window-border">
+                    <main className='window-content'>
+                        {windowContent}
+                    </main>
+                </div>
+            </main>
+        </DndContext>
     )
 }
